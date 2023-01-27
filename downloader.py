4456adea604c6e035e9f3fb7b8a5e5f571f6aa58
andy-1au <andyolau888@gmail.com>
2023-01-27 14:13:13 -0500

    successfully downloaded both audio and video formats in high quality possible using .streams.filter function

--- downloader.py
+++ downloader.py
@@ -1,15 +1,32 @@
 from pytube import YouTube
+from pytube.cli import on_progress # for progress bar in terminal
 
 videoSavePath = "C://Users//andyr//OneDrive - Lehigh University//DESKTOP//Projects//Workstudy//Special-Collections-Youtube-Downloader-Project//Videos Test Folder" #Insert save path for videos here
 
-def Download(link):
-    yt = YouTube(link)
-    try: 
-        print("Downloading...")
-        yt.streams.filter(res="1080p").first().download('C://Users//andyr//OneDrive - Lehigh University//DESKTOP//Projects//Workstudy//Special-Collections-Youtube-Downloader-Project//Videos Test Folder')
-        print("Download complete.")
+audioSavePath = "C://Users//andyr//OneDrive - Lehigh University//DESKTOP//Projects//Workstudy//Special-Collections-Youtube-Downloader-Project//Audios Test Folder" #Insert save path for audio here
+
+def DASHDownload(link): 
+    videoObject = YouTube(link, on_progress_callback=on_progress)
+    audioObject = YouTube(link, on_progress_callback=on_progress)
+    try:
+        print("\nDownloading Video File...")
+
+        videoObject = videoObject.streams.filter(adaptive=True).order_by('resolution').desc().first()
+        print(videoObject) # debug
+        vtag = videoObject.itag 
+        print(str(vtag) + " is the itag of the video stream.") #debug
+        videoObject.download(videoSavePath)
+        
+        print("\nDownloading Audio File..")
+        audioObject = audioObject.streams.filter(only_audio=True).first()
+        print(audioObject) #debug
+        atag = audioObject.itag
+        print(str(atag) + " is the itag of the audio stream.") #debug
+        audioObject.download(audioSavePath)
+      
+        print("\nDownload complete.")
     except:
         print('Error: Unable to download video.')
- 
+
 link = input('Enter the youtube link:')
-Download(link)
+DASHDownload(link)
