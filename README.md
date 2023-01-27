710907e49274b1fedf1a52949da4ee266a1bcba9
andy-1au <andyolau888@gmail.com>
2023-01-27 13:08:36 -0500

    successfully downloaded 1080p and higher videos but no audio, working on DASH download format

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Special-Collections-Youtube-Downloader-Project
+
+
+## Useful Links/Resources
+- https://www.freecodecamp.org/news/python-program-to-download-youtube-videos/
--- downloader.py
+++ downloader.py
@@ -1,14 +1,15 @@
 from pytube import YouTube
 
+videoSavePath = "C://Users//andyr//OneDrive - Lehigh University//DESKTOP//Projects//Workstudy//Special-Collections-Youtube-Downloader-Project//Videos Test Folder" #Insert save path for videos here
+
 def Download(link):
-    youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
-    try:
-        youtubeObject.download()
+    yt = YouTube(link)
+    try: 
+        print("Downloading...")
+        yt.streams.filter(res="1080p").first().download('C://Users//andyr//OneDrive - Lehigh University//DESKTOP//Projects//Workstudy//Special-Collections-Youtube-Downloader-Project//Videos Test Folder')
+        print("Download complete.")
     except:
-        print("An error has occurred")
-    print("Download is completed successfully")
-
-
-link = input("Enter the YouTube video URL: ")
+        print('Error: Unable to download video.')
+ 
+link = input('Enter the youtube link:')
 Download(link)
