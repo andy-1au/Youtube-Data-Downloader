4a8ede66328d135ffc5846c9cfe0dfed08507d2b
andy-1au <andyolau888@gmail.com>
2023-02-21 11:43:04 -0500

    working on the transcribe function and it's accuracy, seeing if mp4 or mp3 file matters

--- Scripts/Transcribe.py
+++ Scripts/Transcribe.py
@@ -1,9 +1,12 @@
 import whisper
-
 import Download as dl   # Download.py
 
 
 model = whisper.load_model("base") # load OpenAI's model
+
+result = model.transcribe("Audios Folder\mJoTBkLkVyg.mp4")
+print(result["text"])
+
 
 def transcribe(videoPath, outputPath):
     result = model.transcribe(videoPath)
@@ -11,9 +14,3 @@ def transcribe(videoPath, outputPath):
     with open(outputPath, 'w') as f:
         f.write(lines)
 
-result = model.transcribe("Combine Folder\mJoTBkLkVyg.mp4")
-print(result['text'])
-lines = result['text'] # get the text
-outputPath = "Subtitles Folder\subtitle.txt"
-with open(outputPath, 'w') as f:
-    f.write(lines)
